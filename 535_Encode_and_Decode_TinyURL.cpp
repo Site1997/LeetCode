95ea8f5b53f0c3baa456a9a7b744a972e3124a75
Site Li <604335661@qq.com>
2018-04-11 22:03:02 +0800

    Create 535_Encode_and_Decode_TinyURL.cpp

---
+++ 535_Encode_and_Decode_TinyURL.cpp
@@ -0,0 +1,13 @@
+/** Encodes a URL to a shortened URL. */
+char* encode(char* longUrl) {
+    return longUrl;
+}
+
+/** Decodes a shortened URL to its original URL. */
+char* decode(char* shortUrl) {
+    return shortUrl;
+}
+
+// Your functions will be called as such:
+// char* s = encode(s);
+// decode(s);
